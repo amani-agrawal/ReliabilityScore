7e6a996c50baaabe59750e5b7dc5923bcced97da
shreyaasxo <ss14608@nyu.edu>
2025-02-07 00:04:22 -0500

    test

--- frontend/src/minikit-provider.tsx
+++ frontend/src/minikit-provider.tsx
@@ -3,7 +3,7 @@ import { ReactNode, useEffect } from "react";
 
 export default function MiniKitProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
-    MiniKit.install();
+    MiniKit.install("app_b842771510c922e20401ddc0ec09a5e3");
   }, []);
 
   console.log("Is MiniKit installed correctly? ", MiniKit.isInstalled());
